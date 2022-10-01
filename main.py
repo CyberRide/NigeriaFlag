242e7ffd9d62f13f3b49fc4fdb69016e83e4aec7
CyberRide <59187481+CyberRide@users.noreply.github.com>
2022-10-01 19:55:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,92 @@
-#Nigeria Flag Using Python
-
-from PIL import Image, ImageDraw
+#Nigeria Flag Using Python Created By CyberRide
+import colorsys
 import turtle
+from turtle import *
 
-green = (0, 128, 0)
-white = (255, 255, 255)
-green = (0, 128, 0)
+wn = turtle.Screen()
+wn.bgcolor("gold")
 
-def nigeria (fname, width=400, height=300):
-    flag= Image.new("RGB", (width, height))
-    p,q= width//3, 2 * width //3
-    draw= ImageDraw.Draw(flag)
-    draw.rectangle((0,0,p,height),green)
-    draw.rectangle((p,0,q,height),white)
-    draw.rectangle((q,0,width,height),green)
-    flag.save(fname)
-nigeria("nigeria.gif")
-tr= turtle.Turtle()
-wm= turtle.Screen()
-turtle.bgcolor('light green')
-wm.addshape('nigeria.gif')
-tr.shape('nigeria.gif')
-wm.mainloop()
+turtle = turtle.Turtle()
+
+turtle.speed(2)
+turtle.penup()
+turtle.shape("turtle")
+
+setup(700, 500)
+def draw(x,y,length,width,color):
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(length)
+    turtle.right(90)
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(length)
+    turtle.right(90)
+    turtle.end_fill()
+    turtle.penup()
+def green():
+    x=-350
+    y = 250
+    color = "green"
+    draw(x,y,500,270,color)
+def white():
+    x=-150
+    y=250
+    color = "white"
+    draw(x,y,500,270,color)
+def green2():
+    x=100
+    y=250
+    color = "green"
+    draw(x,y,500,270,color)
+def cy():
+    turtle.penup()
+    turtle.goto(-200, -100)
+    turtle.pencolor("white")
+    turtle.write("Created by CyberRide", font=("Helevetica", 34, "bold"))
+def cys():
+    turtle.penup()
+    turtle.goto(-150, 1)
+    turtle.pencolor("white")
+    turtle.write("Nigeria Flag", font=("Helevetica", 34, "bold"))
+def happy():
+    turtle.penup()
+    turtle.goto(-130, -145)
+    turtle.pencolor("green")
+    turtle.write("🄷🄰🄿🄿🅈", font=("Helevetica", 54, "bold"))
+def independence():
+   turtle.penup()
+   turtle.goto(-150, -165)
+   turtle.pencolor("green")
+   turtle.write("🄸🄽🄳🄴🄿🄴🄽🄳🄴🄽🄲🄴", font=("Helevetica", 28, "bold"))
+def day():
+   turtle.penup()
+   turtle.goto(-90, -225)
+   turtle.pencolor("green")
+   turtle.write("🄳🄰🅈", font=("Helevetica", 54, "bold"))
+    
+def coat():
+    wn.addshape("f.gif")
+    turtle.shape("f.gif")
+    penup()
+    turtle.goto(-20, 25)
+    pendown()
+    wn.mainloop()
+cys() 
+cy()
+green()
+white()
+green2()
+happy()
+independence()
+day()
+coat()
+
+
+
+turtle.hideturtle()
+turtle.getscreen()._root.mainloop()
